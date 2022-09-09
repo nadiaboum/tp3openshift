bc71de492ec90ed9dca9b393f6835d2294201149
nadiaboum <112882157+nadiaboum@users.noreply.github.com>
2022-09-09 06:36:40 +0200

    Delete genMob.py

--- genMob.py
+++
@@ -1,8 +0,0 @@
-import random
-
-def genMob(Nom):
-    PV = random.randint(5, 20)
-    Force = random.randint(3, 8)
-    Armure = random.randint(0, 5)
-    monmonstre = [Nom, PV, Force, Armure]
-    return monmonstre
