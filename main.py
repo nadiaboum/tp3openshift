7417fd5c85354d12085d7bcf3baeb29c32539005
nadiaboum <112882157+nadiaboum@users.noreply.github.com>
2022-09-09 12:14:16 +0200

    Update main.py
    maj

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request , send_file, redirect, url_for, Response, redirect
-from personnage.py import * 
+
 
 
 app = Flask(__name__)
@@ -9,7 +9,7 @@ import os
 @app.route('/', methods=['GET' , 'POST'])
 
 
-def home():
+def menu():
     if request.form:
         compteurtues = 0
         listevaincus = []
