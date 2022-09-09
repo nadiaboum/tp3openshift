700b6aa8e5c0a733972330a42cc29bc06ff8af29
nadiaboum <112882157+nadiaboum@users.noreply.github.com>
2022-09-09 16:11:10 +0200

    maj

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request , send_file, redirect, url_for, Response, redirect
+from personnage import *
+from createmob import *
+from fight import *
+from compteurennemistues import *
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET' , 'POST'])
+
+def menu():
+    if request.form:
+        compteurtues = 0
+        listevaincus = []
+        pseudo = request.form['pseudo']
+
+        monperso = personnage(pseudo, 20,6,3)
+        
+        while monperso[1] > 0:
+            monstreennemi = createMob()
+            fight(monperso, monstreennemi)
+
+            if monperso[1] > 0:
+                compteurtues = compteurennemistues(compteurtues)
+                listevaincus.append(monstreennemi[0])
+        return render_template("index.html", pseudo=pseudo, compteurtues=compteurtues, listevaincus=listevaincus)
+    return render_template("index.html")
+    
+
+if __name__== "__main__":
+     app.run(debug=True, host="0.0.0.0", port=8001)       
